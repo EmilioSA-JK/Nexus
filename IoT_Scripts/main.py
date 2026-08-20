97eb9d9f671497d554c6284cfaf6d09a6dcf78d0
AMD <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-20 11:35:29 -0600

    The final python script using switches

--- IoT_Scripts/main.py
+++ IoT_Scripts/main.py
@@ -10,18 +10,24 @@ def main():
 	# The initial mode of the pins is defined
 	pinMode(0, OUT)
 	pinMode(2, IN)
-	
-	# Here we create a loop lo check the state of the button
+	pinMode(3, IN)
+	pinMode(1, OUT)
+	# Here we create a loop to check the state of the switch continuously
 	while True:
+		#Here we define the conditional to check the switch status
+		#Depending on the status "High" or "Low" the condition will write a state for the IoT device
 		if digitalRead(2) == HIGH:
 			customWrite(0, '1')
 			
 		else:
 			customWrite(0, '0')
 			
+		if digitalRead(3) == HIGH:
+			customWrite(1, '2')
+			
+		else:
+			customWrite(1, '0')
 
 	# The main function is called
 if __name__ == "__main__":	
 	main()
-
-
